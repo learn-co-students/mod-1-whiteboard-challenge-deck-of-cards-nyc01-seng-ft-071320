1dda0dd63d18869db266499716a49d354985053a
redazures <jwu_nyc@yahoo.com>
2020-07-13 12:20:48 -0400

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,48 @@
 class Deck
+    
+    def initialize
+        @deck=[]
+        ranks = %w{A 2 3 4 5 6 7 8 9 20 J Q K}
+        suits = %w{Hearts Clubs Diamonds Spades}
+        suits.each do |suit|
+            ranks.each do|cardio|
+            @deck<<Card.new(cardio,suit)
+            end
+        end
+        @deck
+    end
+
+    def cards 
+        @deck
+    end
+
+    def display_deck
+        puts "#{@deck}"
+    end
+
+    def count_deck
+        p @deck.count
+    end
+
+    def choose_card
+        @deck.shift
+    end
 
 end
 
-class Card
+class Card 
+
+    def initialize (suit, rank)
+        @suit=suit
+        @rank=rank
+    end
+
+    def rank
+        @rank
+    end
+
+    def suit
+        @suit
+    end
 
 end
